77df58b1ef51c31f1659e54c3d3c4856354373c0
murlov <amolovv@gmail.com>
2024-12-08 01:36:27 +0300

    "Put updating"

--- src/main/java/com/thehecklers/sbur_rest_demo/SburRestDemoApplication.java
+++ src/main/java/com/thehecklers/sbur_rest_demo/SburRestDemoApplication.java
@@ -2,6 +2,8 @@ package com.thehecklers.sbur_rest_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -81,15 +83,19 @@ class RestApiDemoController {
 	}
 
 	@PutMapping("/{id}")
-	Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
 		int coffeeIndex = -1;
+
 		for (Coffee c: coffees) {
 			if (c.getId().equals(id)) {
 				coffeeIndex = coffees.indexOf(c);
 				coffees.set(coffeeIndex, coffee);
 			}
 		}
-		return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+
+		return (coffeeIndex == -1) ?
+				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+				new ResponseEntity<>(coffee, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{id}")
